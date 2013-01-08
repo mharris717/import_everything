3ba1b53e2dae919ad043e480ffa9d5735c52c121
Mike Harris <mharris717@gmail.com>
2013-01-08 14:43:51 -0500

    vol

---
+++ vol/filename_meth.rb
@@ -0,0 +1,4 @@
+load "lib/import_everything.rb"
+
+p = ImportEverything::CsvParser.new
+p.filename = "abc"
---
+++ vol/web_test.rb
@@ -0,0 +1,38 @@
+require 'json'
+require 'pp'
+require 'net/http'
+load "lib/import_everything.rb"
+
+csv = "a,b,c
+1,2,3
+4,5,6"
+
+def get_parsed(body,filename=nil)
+  uri = URI('http://localhost:4000/get')
+  params = { :body => body, :filename => filename }
+  uri.query = URI.encode_www_form(params)
+
+  res = Net::HTTP.get_response(uri)
+  rows = JSON.parse(res.body)
+end
+
+
+rows = get_parsed csv
+puts rows.class
+pp rows
+
+sql = "insert into widgets (color,price) values ('Black',10);"
+sql = File.read("spec/players.sql")
+
+rows = get_parsed sql, "widgets.sql"
+pp rows
+
+if false
+  
+
+  parser = ImportEverything::SqlInsertParser::LineParser.new(:line => sql)
+  puts parser.value_hash.inspect
+
+  parser = ImportEverything::SqlInsertParser.new(:str => sql, :filename => "abc.sql", :file => nil)
+  puts parser.value_hashes.inspect
+end
